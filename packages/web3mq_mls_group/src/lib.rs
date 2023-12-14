e25072611c740f051297e53585440e08e7b56adf
tommy <batxent@me.com>
2023-12-14 16:22:48 +0800

    clean

--- packages/web3mq_mls_group/src/lib.rs
+++ packages/web3mq_mls_group/src/lib.rs
@@ -2,17 +2,3 @@ pub mod service;
 mod storage;
 // private
 mod file_helpers;
-
-// use wasm_bindgen::prelude::*;
-
-// #[wasm_bindgen]
-// extern "C" {
-//     pub fn alert(s: &str);
-// }
-
-// #[wasm_bindgen]
-// pub fn greet(name: &str) {
-//     alert(&format!("Hello, {}!", name));
-// }
-
-
